ec4c747816f3c3c3c84811fda2b854812f30ff4d
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2024-02-11 13:41:51 +0530

    Basic of variables.

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        int myFirstVaribles = 1000;
+        System.out.println(myFirstVaribles);
+        myFirstVaribles =10000;
+        System.out.println(myFirstVaribles);
+        String myFirstStringVariables = "My 1st string variables";
+        System.out.println(myFirstStringVariables);
+        myFirstVaribles = 10+15-2;
+        System.out.println(myFirstVaribles);
+        myFirstVaribles = (10 + 5) + (2 * 10);
+        System.out.println(myFirstVaribles);
+
+
+    }
+}
